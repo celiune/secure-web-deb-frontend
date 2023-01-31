f9be4072aeea6835bc4e38fda70a656a420423bc
celiune <celineliu1811@gmail.com>
2023-01-31 14:06:36 +0100

    login page

---
+++ src/routes/login/+page.server.js
@@ -0,0 +1,44 @@
+import { fail, redirect } from '@sveltejs/kit';
+import * as api from '../lib/api.js.js';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ locals }) {
+	if (locals.user) throw redirect(307, '/');
+}
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+	login: async ({ cookies, request }) => {
+		const data = await request.formData();
+		const username = data.get('username');
+		const password = data.get('password');
+
+		if (!username) { 
+            return fail ( 400, { username: 'Missing', error: 'Username is required' });
+        }
+          
+        if (!password) {
+            return fail ( 400, { password: 'Missing', error: 'Password is required' });
+        }
+        
+		const body = await api.post('users/login', { username, password });
+
+        if (!body.result) {
+            return fail(401, { error: 'Login failed, please try again' });
+        }        
+
+		if (body.errors) {
+			return fail(401, body);
+		}
+
+		else {
+			const value = body.result.jwt
+			cookies.set('jwt', value, { path: '/' });
+			return { success: true };
+		}
+	},
+
+	register: async (event) => {
+		// TODO register the user
+	}
+};
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+    import { enhance } from '$app/forms';
+
+    /** @type {import('./$types').ActionData} */  
+    export let form
+
+</script>
+
+<form method="post" action="?/login" use:enhance>
+    <label>
+        Username
+        <input 
+            id="username"
+            name="username" 
+            type="username" 
+            value={form?.username ?? ''} 
+            required
+        > 
+    </label>
+    <label>
+        Password
+        <input 
+            id="password"
+            name="password" 
+            type="password"
+            required
+        >
+    </label>
+
+    <button type="submit">Log In</button>
+
+    <button formaction="?/register">Register</button>
+
+    {#if form?.missing}
+    <p class="error">The username field is required</p>
+    {/if}
+
+    {#if form?.incorrect}
+    <p class="error">Invalid credentials!</p>
+    {/if}
+
+    {#if form?.success}
+    <!-- this message is ephemeral; it exists because the page was rendered in response to a form submission. it will vanish if the user reloads -->  
+    <p>Successfully logged in! Welcome back, {form.username}</p>
+    {/if}
+</form>
+
