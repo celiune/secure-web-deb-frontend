44b3e1109e6b1b2ac4cc15d2d6d0c8b803a69ce1
celiune <celineliu1811@gmail.com>
2023-02-07 21:59:45 +0100

    Register page, set cookies

--- src/routes/register/+page.server.js
+++ src/routes/register/+page.server.js
@@ -1,6 +1,5 @@
 import { fail, redirect } from '@sveltejs/kit';
 import * as api from '$lib/api';
-const bcrypt = require('bcrypt')
 
 
 /** @type {import('./$types').PageServerLoad} */
@@ -10,38 +9,40 @@ export async function load({ locals }) {
 
 /** @type {import('./$types').Actions} */
 export const actions = {
-    register: async ({ request, cookies }) => {
+    register: async ({ locals,request, cookies }) => {
         const data = await request.formData();
         const username = data.get('username');
         const password = data.get('password');
         const confirmPassword = data.get('confirmPassword');
 
         if (!username) {
-            return fail(400, { username: 'Missing', error: 'Username is required' });
+            return fail(400, { missing:true, error: 'Username is required' });
         }
 
         if (!password) {
-            return fail(400, { password: 'Missing', error: 'Password is required' });
+            return fail(400, { missing:true, error: 'Password is required' });
         }
 
         if (!confirmPassword) {
-            return fail(400, { confirmPassword: 'Missing', error: 'Confirm password is required' });
+            return fail(400, { missing:true, error: 'Confirm password is required' });
         }
 
         if (password !== confirmPassword) {
-            return fail(400, { password: 'Mismatch', error: 'Passwords do not match' });
+            return fail(400, { mismatch:true, error: 'Passwords do not match' });
         }
 
-        const body = await api.post('users/register', { username, password });
+        let body = await api.post('users/register', { username, password });
 
-        if (body.errors) {
-            return fail(401, body);
+        if (!body.ok) {
+            return fail(400, {invalidUsername:true, error: 'Username already taken'});
         }
 
         else {
+            body = await api.post('users/login', {username,password});
             const value = body.result.jwt
-            cookies.set('jwt', value, { path: '/' });
-            return { success: true };
+			cookies.set('jwt', value, { path: '/' });
+			locals.user = body.result
+            return { success: true, ...body.result };
         }
     }
 };
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -6,49 +6,33 @@
 
 </script>
 
-<form method="post" use:enhance>
-    <label>
-        Email
-        <input 
-            id="email"
-            name="email" 
-            type="email" 
-            value={form?.email ?? ''} 
-            required
-        > 
-    </label>
-    <label>
-        Username
+<div class="container">
+<form method="post" use:enhance action="?/register">
+    <h1>Sign up</h1>
         <input 
             id="username"
             name="username" 
-            type="username" 
+            type="text" 
             value={form?.username ?? ''} 
-            required
+            placeholder="Username"
         > 
-    </label>
-    <label>
-        Password
         <input 
             id="password"
             name="password" 
             type="password"
-            required
+            placeholder="Password"
         >
-    </label>
-    <label>
-        Confirm Password
         <input 
             id="confirmPassword"
             name="confirmPassword" 
             type="password"
-            required
+            placeholder="Confirm Passsword"
         >
-    </label>
+    <button type="submit" >Register</button>
 
-    <button type="submit">Register</button>
+    <p class="message">Already registered? <a href="/login">Log In</a></p>
 
-    <button formaction="?/login">Log In</button>
+
 
     {#if form?.missing}
     <p class="error">Please fill out all fields</p>
@@ -58,8 +42,86 @@
     <p class="error">Passwords do not match</p>
     {/if}
 
+    {#if form?.invalidUsername}
+    <p class="error">Username already taken</p>
+    {/if}
+
     {#if form?.success}
     <!-- this message is ephemeral; it exists because the page was rendered in response to a form submission. it will vanish if the user reloads -->  
     <p>Successfully registered! Welcome, {form.username}</p>
     {/if}
 </form>
+</div>
+
+
+<style>
+    @import url(https://fonts.googleapis.com/css?family=Roboto:300);
+    
+    .container{
+        width:400px;
+        margin:0 auto;
+        margin-top:5%;
+        background: linear-gradient(to bottom, #33ccff, #ff99cc);
+    }
+    /* Bordered form */
+    form {
+        height: 425px;
+        width:100%;
+        padding: 30px;
+        border: 1px solid #f1f1f1;
+        background: #fff;
+        box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+    }
+    .container h1{
+        width: 30%;
+        margin: 0 auto;
+        padding-bottom: 10px;
+    }
+    
+    /* Full-width inputs */
+    input[type=text], input[type=password] {
+        width: 100%;
+        padding: 20px 30px;
+        margin: 22px 0 10px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        box-sizing: border-box;
+    }
+    
+    input[type=password] {
+        width: 100%;
+        padding: 20px 30px;
+        margin: 10px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        box-sizing: border-box;
+    }
+    
+    /* Set a style for all buttons */
+    button{
+        background-color: #33ccff;
+        color: white;
+        padding: 20px 30px;
+        margin: 10px 0;
+        border: none;
+        cursor: pointer;
+        width: 100%;
+    }
+    button:hover {
+        background-color: white;
+        color: #33ccff;
+        border: 1px solid #33ccff;
+    }
+    
+    form .message {
+      margin: 15px 0 0;
+      color: #b3b3b3;
+      font-size: 15px;
+    }
+    form .message a {
+      color: #ff99cc;
+      text-decoration: none;
+    } 
+    
+    
+    </style>
