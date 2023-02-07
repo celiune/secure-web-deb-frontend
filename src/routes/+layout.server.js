471c0345ddf99f5bad58dca3130a8c2020ac0d54
celiune <celineliu1811@gmail.com>
2023-02-07 22:06:59 +0100

    Layout files, load users username and role, background style

---
+++ src/routes/+layout.server.js
@@ -0,0 +1,9 @@
+/** @type {import('./$types').LayoutServerLoad} */
+export function load({ locals }) {
+	return {
+		user: locals.user && {
+			username: locals.user.username,
+			role: locals.user.role
+		}
+	};
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,24 @@
-<form method="POST" action="/login?/register">
-    <!-- content -->
-</form>
+<script >
+	import Nav from './Nav.svelte';
+	/** @type {import('./$types').LayoutData} */
+	export let data;
+</script>
+<body>
+
+<Nav {data} />
+
+<main >
+	<slot />
+</main>
+</body>
+
+<style>
+	body{
+		background: linear-gradient(to bottom, #33ccff, #ff99cc);
+		background-repeat: no-repeat;
+		background-attachment: fixed;
+		height: 100%;
+		margin: 0;
+		padding: 1rem;
+	}
+</style>
